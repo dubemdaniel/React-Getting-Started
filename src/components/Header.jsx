46cf28f60fe93107358201c055df12c608db420c
Daniel Chukwudubem <princedaniel7890@gmail.com>
2023-06-28 21:34:19 +0100

    gettingStarted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import keyConceptsImage from "../assets/images/key-concepts.png";
 
 const Header = () => {
   return (
-    <header className="center">
+    <header>
       <img src={keyConceptsImage} alt="Medal badge with a star" />
       <h1>Key React Concepts</h1>
       <p>Selected key React concepts you should know about</p>
